20b10a3aa8a60020a36188367f5d10351f16361b
dbsuarez <38133300+dbsuarez@users.noreply.github.com>
2022-07-20 01:42:31 +0800

    027

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MyApp(),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          backgroundColor: Colors.blueGrey[900],
+          title: const Text('I Am Rich'),
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage('https://appwrite.io/images-ee/hero-light.png'),
+          ),
+        ),
+      ),
+    );
+  }
+}
